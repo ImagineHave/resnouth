28c5b1e36ab370e130a61e63fbb8fe4ba3112ecb
oyvind <oyvind.woller@imagine-have.xyz>
2018-04-19 20:59:34 +0100

    restructure to use module

--- application/app.py
+++ application/app.py
@@ -2,9 +2,7 @@ from flask import Flask, request, jsonify
 import os
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
-
-
-app = Flask(__name__)
+from application import app
 
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 
