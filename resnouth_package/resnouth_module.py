620165146d9495d6cfddfc3fe44d1ea7233651c6
oyvind <oyvind.woller@imagine-have.xyz>
2018-04-20 07:09:25 +0100

    fix incorrect jwt import

--- resnouth_package/resnouth_module.py
+++ resnouth_package/resnouth_module.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import os
 from flask_jwt_extended import JWTManager
-from flask_jwt_extended import (jwt_required, jwt_access_token_required)
+from flask_jwt_extended import (jwt_required)
 
 resnouth_instance = Flask(__name__)
 
@@ -10,6 +10,6 @@ resnouth_instance.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 jwt = JWTManager(resnouth_instance)
 
 @resnouth_instance.route('/protectedResouce', methods=['GET'])
-@jwt_access_token_required
+@jwt_required
 def getProtectedResource():
     return jsonify({'resouce': 'This is the protected resource'})
